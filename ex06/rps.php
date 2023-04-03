0e55ac9b63f98ebabcd8f025e4169881988edcd5
Thfirmin <thfirmin.dsantos@gmail.com>
2023-04-03 13:13:42 -0300

    Finalized ex06

--- ex06/rps.php
+++ ex06/rps.php
@@ -1,13 +1,39 @@
 #!/usr/bin/php
 <?php
+# Declaring fiunction
+function getIdx($ch, $opt)
+{
+	if ($ch == $opt[0])
+		return 0;
+	elseif ($ch == $opt[1])
+		return 1;
+	elseif ($ch == $opt[2])
+		return 2;
+	else
+		return -1;
+}
+# Preamble output
+print("Choose rock, paper, or scissors: ");
+
 /*
  * Declaring variable ($varName = value;)
  * > The type of variable will be setted in real time until you use it
- * */
-
-# Preamble output
-print("Choose rock, paper, or scissors: ");
-# Taking input form STDION using fgets and trimming the input using trim
-$chose = trim(fgets(STDIN));
-echo ("chose = \"$chose\"\n");
+ *
+ * Taking input from STDIN using fgets;
+ * Trimming input using thim;
+ * Lowercasing trimmed input using strtolower
+ */
+$opt = array("rock", "paper", "scissors");
+$chose = strtolower(trim(fgets(STDIN)));
+$chId = getIdx($chose, $opt);
+if ($chId > -1)
+{
+	do
+		$enemy = (rand(1, 42) % 3);
+	while ($enemy == $chId);
+	if ($enemy == (($chId + 1) % 3))
+		echo "Sorry, you lost. The computer chose $opt[$enemy].\n";
+	else
+		echo "Congratulations! You won! The computer chose $opt[$enemy].\n";
+}
 ?>
